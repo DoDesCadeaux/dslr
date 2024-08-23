94fa9cbfa38e87b3595a3c74844e753ebbc36fc0
dodescadeaux <dduraku@student.s19.be>
2024-08-23 14:00:55 +0200

    V.2.1 Histogram OK

---
+++ histogram.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+path = 'datasets/dataset_train.csv'
+
+data = pd.read_csv(path)
+print(data.columns)
+
+courses = ['Arithmancy', 'Astronomy', 'Herbology', 'Defense Against the Dark Arts', 'Divination',
+           'Muggle Studies', 'Ancient Runes', 'History of Magic', 'Transfiguration', 'Potions',
+           'Care of Magical Creatures', 'Charms', 'Flying']
+
+try:
+    plt.figure(figsize=(20, 15))
+
+    for i, course in enumerate(courses, 1):
+        plt.subplot(4, 4, i)
+        sns.histplot(data=data, x=course, hue='Hogwarts House', multiple='stack')
+
+    plt.show()
+except KeyboardInterrupt:
+    exit(0)
