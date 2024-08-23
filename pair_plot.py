d606709608e0be5386e96c155e39f6a382b74f69
dodescadeaux <dduraku@student.s19.be>
2024-08-23 15:35:52 +0200

    scatter_plot + pair_plot OK

--- pair_plot.py
+++ pair_plot.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sys import argv
+from sklearn.model_selection import train_test_split
 
 if __name__ == '__main__':
     dataset_folder = 'datasets/'
@@ -9,21 +10,15 @@ if __name__ == '__main__':
     try:
         path = dataset_folder + argv[1]
         data = pd.read_csv(path)
+        sample_data, _ = train_test_split(data, test_size=0.9, stratify=data['Hogwarts House'], random_state=42)
     except (IndexError, FileNotFoundError) as e:
         print(e)
         exit(1)
 
     try:
-        course_1 = argv[2]
-        course_2 = argv[3]
-    except IndexError as e:
+        sns.pairplot(data=sample_data, hue='Hogwarts House', corner=True)
+        plt.show()
+    except KeyboardInterrupt as e:
         print(e)
         exit(2)
 
-    try:
-        plt.figure(figsize=(15, 6))
-        sns.scatterplot(data=data, x=f'{course_1}', y=f'{course_2}', hue='Hogwarts House')
-        plt.show()
-    except (ValueError, KeyboardInterrupt) as e:
-        print(e)
-        exit(3)
